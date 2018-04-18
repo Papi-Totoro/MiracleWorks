0cc1728162b329644705299118b01b4195d5aa48
Totoro <charliebigshow@hotmail.com>
2018-04-18 00:35:19 -0500

    Este es un commint de prueba

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+    
+    <head>
+        
+        <title>"ShareBooks"</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="CSS/Estilo.css">
+        <link rel="stylesheet" type="text/css" href="CSS/W3.css">   
+              
+    </head>
+    
+    <body>
+        
+        <div class="w3-bar w3-black" style="width=100%;">
+            <div class="w3-dropdown-hover w3-bar-item w3-black">
+                <button class="w3-button w3-black">Ajustes</button>
+                <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                <a class="w3-bar-item w3-button">Iniciar Sesion</a>
+                <a href="#" class="w3-bar-item w3-button">Registrarse</a>
+                </div>
+            </div>
+            <div class="w3-bar-item w3-left">
+            <input type="search" name="search" placeholder="Buscar" class="animated-search-form">
+            </div>
+            <div class="w3-bar-item w3-left">
+            <a href="Registrar.php" class="w3-bar-item w3-button">Registrarse</a>
+            <a href="IniciarSesion.php" class="w3-bar-item w3-button w3-right">Iniciar Sesion</a>
+            </div>
+        </div>
+        
+    </body>
+
+</html>
